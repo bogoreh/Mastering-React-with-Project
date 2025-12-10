86b396f2a30581e6fa5d2fbe523ed07cafddb6f7
bogoreh <76124689+bogoreh@users.noreply.github.com>
2025-12-10 13:19:39 +0300

    first initial

---
+++ 097_Multiplayer Chess/src/components/Footer.tsx
@@ -0,0 +1,35 @@
+import React from 'react';
+import styled from 'styled-components';
+
+const FooterContainer = styled.div`
+  display: flex;
+  flex-direction: row;
+  flex-wrap: wrap;
+`;
+
+const FooterText = styled.p`
+  font-weight: normal;
+  justify-self: center;
+  text-align: left;
+  color: ${props => props.theme.colors.text};
+`;
+
+const FooterSpacer = styled.div`
+  flex-grow: 1;
+  overflow: hidden;
+`;
+
+const FooterLink = styled.a`
+  color: ${props => props.theme.colors.text};
+`;
+
+export const Footer: React.FC = () => {
+  return (
+    <FooterContainer>
+      {/*<FooterText>
+        2025 Abdibogoreh
+      </FooterText>*/}
+      <FooterSpacer />
+    </FooterContainer>
+  );
+};
